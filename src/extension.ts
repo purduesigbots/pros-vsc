a061727c70bb1764abfb5b37951d26e3e8669ad7
Kunwar Sahni <kunwar.sahni01@gmail.com>
2021-05-15 08:17:41 -0400

    Add terminal

--- src/extension.ts
+++ src/extension.ts
@@ -8,6 +8,12 @@ export function activate(context: vscode.ExtensionContext) {
 	vscode.commands.registerCommand('pros.helloWorld', () => vscode.window.showInformationMessage('Hello World from pros!'));
 	// vscode.commands.registerCommand('pros.upload', () => uploadProject(undefined, vscode.workspace.workspaceFolders[0].uri.path));
 	vscode.commands.registerCommand('pros.upload', () => vscode.window.setStatusBarMessage('Uploading...', 5000));
+	const terminal = vscode.window.createTerminal('PROS Terminal');
+
+	vscode.commands.registerCommand('pros.terminal', () => {
+		terminal.show();
+		terminal.sendText("pros terminal");
+	});
 
 	vscode.commands.registerCommand('pros.welcome', () => {
 		const panel = vscode.window.createWebviewPanel(
