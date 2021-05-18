363537d79eb7568fc05bdca3010c144331ec4511
Jonathan Bayless <baylessjonathan@gmail.com>
2021-05-17 21:39:12 -0400

    currently bypassing the middleware

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,9 @@
 import * as vscode from 'vscode';
-import { uploadProject } from '@purduesigbots/pros-cli-middleware';
+import * as child_process from 'child_process';
+import * as path from 'path';
+import * as fs from 'fs';
+
+import { uploadProject, createNewProject } from '@purduesigbots/pros-cli-middleware';
 import { TreeDataProvider } from './views/tree-view';
 import { getWebviewContent } from './views/welcome-view';
 
@@ -37,6 +41,80 @@ export function activate(context: vscode.ExtensionContext) {
 	vscode.commands.registerCommand('pros.upgrade', () => {
 		terminal.show();
 		terminal.sendText("pros conduct upgrade");
+	});
+
+	vscode.commands.registerCommand('pros.new', async () => {
+		// directory selection dialog
+		const directoryOptions: vscode.OpenDialogOptions = {
+			canSelectMany: false,
+			title: "Select a directory where the PROS Project will be created",
+			openLabel: "Create Project Here",
+			canSelectFolders: true,
+			canSelectFiles: false
+		};
+		const uri: string | undefined = await vscode.window.showOpenDialog(directoryOptions).then((uri) => {
+			return uri ? uri[0].fsPath : undefined;
+		});
+		if (uri === undefined) {
+			// error msg
+			return;
+		}
+
+		// select target
+		const targetOptions: vscode.QuickPickOptions = {
+			placeHolder: "v5",
+			title: "Select the target device"
+		};
+		const platform = await vscode.window.showQuickPick(["v5", "cortex"], targetOptions);
+		if (platform === undefined) {
+			return;
+		}
+
+		// set project name
+		const projectNameOptions: vscode.InputBoxOptions = {
+			prompt: "Project Name",
+			placeHolder: "my-pros-project"
+		};
+		let projectName = await vscode.window.showInputBox(projectNameOptions);
+		if (!projectName) {
+			projectName = "my-pros-project";
+		}
+
+		// set kernel version
+		const kernelOptions: vscode.QuickPickOptions = {
+			placeHolder: "latest",
+			title: "Select the project version"
+		};
+		const version = await vscode.window.showQuickPick(["latest", "3.1.3"], kernelOptions);
+		if (version === undefined) {
+			return;
+		}
+
+		// install default libraries
+		// await createNewProject({
+		// 	notify: () => {},
+		// 	log: () => {},
+		// 	prompt: () => {},
+		// 	finalize: (data) => {
+		// 		vscode.window.showInformationMessage(`${data}`);
+		// 	},
+		// 	input: () => {},
+		// }, uri, version, platform).then((code) => {
+		// 	vscode.window.showInformationMessage(`Here: ${code}`);
+		// }).catch((error) => {
+		// 	vscode.window.showInformationMessage(`Fail: ${error}`);
+		// });
+		console.log(projectName);
+		const projectPath = path.join(uri, projectName);
+		console.log(projectPath);
+		await fs.promises.mkdir(projectPath, {recursive: true});
+		child_process.exec(`pros c n ${projectPath} --machine-output`, (err, stdout, stderr) => {
+			console.log('stdout: ' + stdout);
+			console.log('stderr: ' + stderr);
+			if (err) {
+				console.log('error: ' + err);
+			}
+		});
 	});
 
 	vscode.commands.registerCommand('pros.welcome', () => {
