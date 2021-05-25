35375657f0c17e40f76147429cf76b541911e3f9
Jonathan Bayless <baylessjonathan@gmail.com>
2021-05-24 20:04:58 -0400

    resolve some review comments

--- src/commands/upload.ts
+++ src/commands/upload.ts
@@ -5,39 +5,21 @@ import { promisify } from "util";
 import { parseErrorMessage } from "./cli-parsing";
 
 /**
- * Query the user for the slot on the v5 to upload the project to
- *
- * @returns A slot number (as a string)
- */
-const getUploadSlot = async () => {
-  let slots = [];
-  for (let i = 1; i < 9; i++) {
-    slots.push({ label: `${i}` });
-  }
-  const slot = await vscode.window.showQuickPick(slots, {
-    canPickMany: false,
-    title: "Choose the slot to upload to",
-    placeHolder: "1",
-  });
-  return slot?.label ?? "1";
-};
-
-/**
  * Call the PROS upload CLI command.
  *
  * @param slot The slot number to place the executable in
  */
-const runUpload = async (slot: string) => {
+const runUpload = async () => {
   await vscode.window.withProgress(
     {
       location: vscode.ProgressLocation.Notification,
-      title: "Downloading libraries",
+      title: "Uploading Project",
       cancellable: false,
     },
     async (progress, token) => {
       try {
         const { stdout, stderr } = await promisify(child_process.exec)(
-          `pros u --slot ${slot} --project ${vscode.workspace.workspaceFolders?.[0].uri.path} --machine-output`
+          `pros u --project ${vscode.workspace.workspaceFolders?.[0].uri.path} --machine-output`
         );
 
         await vscode.window.showInformationMessage("Project Uploaded!");
@@ -49,10 +31,8 @@ const runUpload = async (slot: string) => {
 };
 
 export const upload = async () => {
-  const slot = await getUploadSlot();
-
   try {
-    await runUpload(slot);
+    await runUpload();
   } catch (err) {
     await vscode.window.showErrorMessage(err.message);
   }
