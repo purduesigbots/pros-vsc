54b1b904068e6901f35c591ad51508027fdce370
BennyBot <btdavis1603@gmail.com>
2021-12-27 22:42:28 -0600

    One-click work on windows 12-27-21

--- src/install.ts
+++ src/install.ts
@@ -9,15 +9,16 @@ var fetch = require('node-fetch');
 var unzipper = require('unzipper');
 import { getVersion } from '@purduesigbots/pros-cli-middleware';
 import { window, ProgressLocation } from 'vscode';
+import { resolveCliPathFromVSCodeExecutablePath } from "vscode-test";
 //import { glob } from "glob";
 //import { version } from "process";
 var Bunzip = require('seek-bzip');
 var tar = require('tar-fs');
 
 //TOOLCHAIN and CLI_EXEC_PATH are exported and used for running commands.
-
 export var TOOLCHAIN: string;
 export var CLI_EXEC_PATH: string;
+export var PATH_SEP: string;
 export async function install(context: vscode.ExtensionContext) {
     const globalPath = context.globalStorageUri.fsPath;
     var cliVersion = null;
@@ -25,6 +26,29 @@ export async function install(context: vscode.ExtensionContext) {
     var version = await getCliVersion('https://api.github.com/repos/purduesigbots/pros-cli/releases/latest');
     console.log("Current CLI Version: " + version);
 
+    var download_cli = "N";
+    var cli_name = "N";
+    var download_toolchain = "N";
+    var toolchain_name = "N";
+    var system = "windows";
+    if (process.platform === "win32") {
+        system = "windows";
+        PATH_SEP = ";";
+        download_cli = "https://github.com/purduesigbots/pros-cli/releases/download/"+version+"/pros_cli-"+version+"-win-64bit.zip";
+        download_toolchain = "https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-win32.zip";
+    } else if (process.platform === "darwin") {
+        system = "macos";
+        PATH_SEP = ":";
+        download_cli = "https://github.com/purduesigbots/pros-cli/releases/download/"+version+"/pros_cli-"+version+"-macos-64bit.zip";
+        download_toolchain = "https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-mac.tar.bz2";
+    } else if (process.platform === "linux") {
+        system = "linux";
+        PATH_SEP = ":";
+        download_cli = "https://github.com/purduesigbots/pros-cli/releases/download/"+version+"/pros_cli-"+version+"-lin-64bit.zip";
+        download_toolchain = "https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2";
+    }
+    cli_name = "pros-cli-"+system+".zip";
+    toolchain_name = "pros-toolchain-"+system+(system ==="windows" ? ".zip" : ".tar.bz2");
     if (cliVersion === null) {
         const labelResponse = await vscode.window.showQuickPick(
             [{ label: "Install it now!", description: "recommended" }, { label: "No I am good" }],
@@ -37,16 +61,8 @@ export async function install(context: vscode.ExtensionContext) {
         if (labelResponse!.label === "Install it now!") {
             const dirs = await createDirs(context.globalStorageUri.fsPath);
             var response = null;
-            if (process.platform === "win32") {
-                download(context, "https://github.com/purduesigbots/pros-cli/releases/download/"+version+"/pros_cli-"+version+"-win-64bit.zip", "pros-cli-windows.zip");
-                download(context, "https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-win32.zip", "pros-toolchain-windows.zip");
-            } else if (process.platform === "darwin") {
-                download(context, "https://github.com/purduesigbots/pros-cli/releases/download/"+version+"/pros_cli-"+version+"-macos-64bit.zip", "pros-cli-macos.zip");
-                download(context, "https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-mac.tar.bz2", "pros-toolchain-macos.tar.bz2", true);
-            } else if (process.platform === "linux") {
-                download(context, "https://github.com/purduesigbots/pros-cli/releases/download/"+version+"/pros_cli-"+version+"-lin-64bit.zip", "pros-cli-linux.zip");
-                download(context, "https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2", "pros-toolchain-linux.tar.bz2", true);
-            }
+            download(context, download_cli, cli_name, system);
+            download(context, download_toolchain, toolchain_name, system);
         } else {
             vscode.window.showInformationMessage("Install it later!");
         }
@@ -54,26 +70,27 @@ export async function install(context: vscode.ExtensionContext) {
         // User already has the CLI installed
         vscode.window.showInformationMessage("PROS CLI is already Installed!");
     }
-    var system = null;
+    paths(globalPath, system);
+}
 
-    if (process.platform === "win32") {
-        system = "windows";
-    } else if (process.platform === "darwin") {
-        system = "macos";
-    } else if (process.platform === "linux") {
-        system = "linux";
-    }
-    var one_clicked = fs.existsSync(globalPath+"/install/pros-cli-"+system);
+async function paths(globalPath : string, system : string) {
+    var one_clicked = fs.existsSync(path.join(globalPath,"install","pros-cli-"+system));
 
     if (!one_clicked) {
         CLI_EXEC_PATH = "pros";
         TOOLCHAIN = "LOCAL"
     } else {
-        TOOLCHAIN = globalPath + "/install/pros-toolchain-"+system+"/gcc-arm-none-eabi-10.3-2021.10";
-        CLI_EXEC_PATH = globalPath + "/install/pros-cli-"+system+"/pros";        
-    }
-}
 
+        //CLI_EXEC_PATH gets quotes but TOOLCHAIN doesn't because reasons.....
+        TOOLCHAIN = path.join(globalPath, "install","pros-toolchain-"+system,"usr");
+        //Do we put a .exe extension onto the exec path if the user is on windows? right now no.
+        CLI_EXEC_PATH = "\""+path.join(globalPath, "install","pros-cli-"+system)+"\""; 
+        //console.log(TOOLCHAIN);
+        //console.log(CLI_EXEC_PATH);
+    }
+
+    //SHELL = (system=="windows" ? path.join(TOOLCHAIN,"bin","sh.exe") : "None");
+}   
 /*
 
 Code Implemented from clangd source code
@@ -98,8 +115,12 @@ async function createDirs(storagePath: string) {
     return { install: install, download: download };
 }
 
-function download(context: vscode.ExtensionContext, downloadURL: string, storagePath: string, bz2: boolean = false) {
+function download(context: vscode.ExtensionContext, downloadURL: string, storagePath: string, system : string) {
     const globalPath = context.globalStorageUri.fsPath;
+    var bz2 = false;
+    if (downloadURL.includes(".bz2")) {
+        bz2 = true;
+    }
     window.withProgress({
         location: ProgressLocation.Notification,
         title: "Installing: " + storagePath,
@@ -141,8 +162,5 @@ function download(context: vscode.ExtensionContext, downloadURL: string, storage
             }
         }
     });
-
-
-
-
+    paths(globalPath, system);
 }
