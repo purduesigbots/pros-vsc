70ad79ec029ba4242b710b7b56e05f55b0d7467d
BeefSteac <37752683+BeefSteac@users.noreply.github.com>
2022-08-07 18:40:18 -0700

    ✨ Select PROS Project from directory (#64)
    * Updated code to allow users to choose project to work on.
    
    * Updated to match comments
    
    * Allow users to use Command Palate to reopen File Folder
    
    * bugfixes
    
    Co-authored-by: BennyBot <btdavis1603@gmail.com>

--- src/extension.ts
+++ src/extension.ts
@@ -76,14 +76,17 @@ export function activate(context: vscode.ExtensionContext) {
       "pros.isPROSProject",
       isProsProject
     );
-
+    //This checks if user is currently working on a project, if not it allows user to select one
     if (isProsProject) {
       getProsTerminal(context).then((terminal) => {
-        terminal.sendText("pros build-compile-commands");
+       terminal.sendText("pros build-compile-commands");
       });
     }
+    else{
+      chooseProject();
+    }
   });
-
+  
   if (
     vscode.workspace
       .getConfiguration("pros")
@@ -123,7 +126,7 @@ export function activate(context: vscode.ExtensionContext) {
   });
 
   vscode.commands.registerCommand("pros.clean", clean);
-
+  vscode.commands.registerCommand("pros.selectProject", chooseProject);
   vscode.commands.registerCommand("pros.terminal", async () => {
     analytics.sendAction("serialterminal");
     try {
@@ -377,4 +380,89 @@ async function workspaceContainsProjectPros(): Promise<boolean> {
     exists = false;
   }
   return exists;
+}
+//This code calls prosProjects and allows user to choose which pros project to work on
+async function chooseProject(){
+  if (
+    vscode.workspace.workspaceFolders === undefined ||
+    vscode.workspace.workspaceFolders === null
+  )
+  {
+  return;
+  }
+  var array = await prosProjects();
+  if(array.length === 0)
+  {
+    vscode.window.showInformationMessage("No PROS Projects found in current directory!");
+    return;
+  }
+  const targetOptions: vscode.QuickPickOptions = {
+    placeHolder: array[0].name,
+    title: "Select the PROS project to work on",
+  };
+  var folderNames :Array<vscode.QuickPickItem>= [];
+  //Specify type for any
+  for(const f of array)
+  {
+    folderNames.push({label: f[0], description: ''});
+  }
+  //console.log(folderNames);
+  
+  // Display the options to users
+  const target = await vscode.window.showQuickPick(
+    folderNames,
+    targetOptions
+  );
+  if (target === undefined) {
+    throw new Error();
+  }
+  //This will open the folder the user selects
+  await vscode.commands.executeCommand(
+    "vscode.openFolder",
+    vscode.Uri.file(path.join(vscode.workspace.workspaceFolders[0].uri.fsPath,target.label))
+  );
+
+}
+
+//This function will return an array full of folder names containing pros project file
+async function prosProjects(){
+  //Specify type for any later
+  var array :any = [];
+  if (
+    vscode.workspace.workspaceFolders === undefined ||
+    vscode.workspace.workspaceFolders === null
+  ) {
+    console.log(vscode.workspace.workspaceFolders);
+    return array;
+  }
+  console.log(vscode.workspace.workspaceFolders);
+
+    
+   
+    for(const workspace of vscode.workspace.workspaceFolders)
+    {
+      const currentDir = workspace.uri;
+      const folders = (await vscode.workspace.fs.readDirectory(currentDir));
+    for(const folder of folders)
+    {
+    var exists = true;
+    try {
+      // By using VSCode's stat function (and the uri parsing functions), this code should work regardless
+      // of if the workspace is using a physical file system or not.
+      const workspaceUri = vscode.Uri.file(path.join(currentDir.fsPath, folder[0]));
+      const uriString = `${workspaceUri.scheme}:${workspaceUri.path}/${'project.pros'}`;
+      const uri = vscode.Uri.parse(uriString);
+      await vscode.workspace.fs.stat(uri);
+    } catch (e) {
+      console.error(e);
+      exists = false;
+    }
+    if(exists)
+    {
+      array.push(folder);
+    }
+  }
+  }
+  console.log(array);
+  return array;   
 }
