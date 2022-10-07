46116d80caf9fb9ff996af9df7387e5cef8b966c
Coder124-crypto <61634406+Coder124-crypto@users.noreply.github.com>
2022-10-07 16:25:44 -0400

    Simple prototype of output parse

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,9 @@
 import * as vscode from "vscode";
 import * as path from "path";
 import * as os from "os";
+import { BaseCommand } from "./commands/BaseCommand.ts"
+const testcmd: BaseCommand = new BaseCommand();
+testcmd.parse_output(json output);
 import { TreeDataProvider } from "./views/tree-view";
 import {
   getWebviewContent,
