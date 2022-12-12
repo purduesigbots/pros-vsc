ea90642521fa26a2df00ed0b24da3e414cbd648a
Kunwar Sahni <kunwar.sahni01@gmail.com>
2022-12-12 15:06:58 +0100

    Add Set Robot Name and Set Team Number

---
+++ src/commands/set-team-number.ts
@@ -0,0 +1,22 @@
+import * as vscode from "vscode";
+import { BaseCommand, BaseCommandOptions } from "./base-command";
+
+import { selectTeamNumber } from "./command_tools";
+
+export const setTeamNumber = async () => {
+  let teamNumber = await selectTeamNumber("Enter a team number");
+  const setTeamNumberCommandOptions: BaseCommandOptions = {
+    command: "pros",
+    args: ["v5", "set_variable", "teamnumber", `${teamNumber}`],
+    message: "Setting Team Number",
+    requiresProsProject: false,
+  };
+  const setTeamNumberCommand: BaseCommand = new BaseCommand(
+    setTeamNumberCommandOptions
+  );
+  try {
+    await setTeamNumberCommand.runCommand();
+  } catch (err: any) {
+    await vscode.window.showErrorMessage(err.message);
+  }
+};
