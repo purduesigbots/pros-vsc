e16b76bcae341d3f72fd4cab360e564a29f510c8
Coder124-crypto <61634406+Coder124-crypto@users.noreply.github.com>
2023-02-02 20:44:55 -0500

    FIxed main link regex search.

--- src/commands/prosDocs.ts
+++ src/commands/prosDocs.ts
@@ -99,78 +99,10 @@ export async function ParseJSON(keyword: string){
       }
 }
 
-async function loadJsonOld(){
-  var base_url = "https://pros.cs.purdue.edu/v5/api/cpp/";
-}
 
 
-async function loadJSON(){
-  var final_websites = "";
-  const AxiosInstance = axios.create(); // Create a new Axios Instance
- 
-  const url = 'https://purduesigbots.github.io/pros-doxygen-docs/api.html'; // PROS Doxygen url
-  const pros_base_url = "https://purduesigbots.github.io/pros-doxygen-docs/";
-  var variable: any;
-  var final_link = "";
-  // go to and scrape api link
-  // Send an async HTTP Get request to the url
-  await AxiosInstance.get(url,{timeout: 5000})
-    .then( // Once we have data returned ...
-      response => {
-        const html = response.data; // Get the HTML from the HTTP request
-        console.log(html);
-        
-        const link_regex: RegExp = RegExp('<a href="(.+)" class="(.+)">(.+)</a>');
-        const sublink_regex: RegExp = RegExp('<a href="(.+)" class="m-doc-self">(.+)</a>');
-      
 
-
-    // In other environments:
-        const cheerio = require('cheerio');
-        const $ = cheerio.load('ul',html);
-        const link_html = $('<ul>');
-        const links = $('li'); //jquery get all hyperlinks
-        const links_c = $(links[0]).attr('href');
-        const links_cpp = $(links[1]).attr('href'); 
-
-        //load all initial hyperlinks into JSON
-        for (var i:number = 0;i <= links.length; i+=1){
-            var link:String  = $(links[i]).attr("href");
-  
-            json.members.push({name: String($(links[i]).text()),members: [{}], url: $(links[i]).attr("href")});
-        }
-        console.log(json);
-    })
-    .catch(error => {
-      console.log(error.response.data.error);
-    });
-    //travel to each link and scrape function links from link
-    for (var i: number = 0;i<=json.members.length; i+=1 ){
-      var subgroup_url = pros_base_url+ json.members[i].url;
-      if(subgroup_url.length > 8+pros_base_url.length){
-      console.log(subgroup_url);
-      await AxiosInstance.get(subgroup_url,{timeout: 5000})
-    .then( // Once we have data returned ...
-      response => {
-        const html = response.data; // Get the HTML from the HTTP request
-        console.log(html);
-
-    // In other environments:
-        const cheerio = require('cheerio');
-        const $ = cheerio.load(html);
-        const sub_links = $('a'); //jquery get all hyperlinks
-        //load all initial hyperlinks into JSON
-        for (var i:number = 0;i <= sub_links.length; i+=1){
-            json.members[i].members.push({"name:":  $(sub_links[i]).text(),"url": subgroup_url+$(sub_links[i]).attr('href')});
-        }
-        console.log(json);
-    });
-    }
-  }
-    
-  console.log(final_link+"Printed");
-  
- }
+var loadJsonFinished = false;
 // most recent json loading function
 // needed for either website
 // uses regex to parse html
@@ -182,9 +114,10 @@ async function loadJSON(){
   const pros_base_url = "https://purduesigbots.github.io/pros-doxygen-docs/";
   var variable: any;
   var final_link = "";
-  const link_regex: RegExp = RegExp('<a href="(.{0,30})" class="m-doc">(.{0,35})<\/a>');
-  const sublink_regex: RegExp = RegExp('<a href="(.{0,30})" class="m-doc-self">(.{0,30})<\/a>');
-  var main_links = [];
+  const link_regex = /<a href="(.{0,30})" class="m-doc">(.{0,35})<\/a>/g;
+  const sublink_regex = /<a href="(#.{0,60})" class="(m-doc-self|m-doc)">(.{0,30})<\/a>/g;
+  var main_links: [string]= [""];
+  main_links.pop();
   var function_links = [];
   //go to and scrape api homepage
   // Send an async HTTP Get request to the url
@@ -192,26 +125,28 @@ async function loadJSON(){
     .then( // Once we have data returned ...
       response => {
         const html = response.data; // Get the HTML from the HTTP request
-        console.log(html);
+        //console.log(html);
         
         
         var links = [];
         var m;
+        
         do {
           m = link_regex.exec(html);
           if (m) {
-              console.log(m[0], m[1], m[2]);
+              //console.log(m[0], m[1], m[2],m[-1]);
               main_links.push(m[1]); 
           }
       } while (m);
       });
       // Go to class links and scrape functions from each link
         for (var i: number = 0; i < main_links.length; i+= 1){
-          await AxiosInstance.get(url,{timeout: 5000})
+          var sublink_html=pros_base_url+main_links[i]
+          await AxiosInstance.get(sublink_html,{timeout: 5000})
     .then( // Once we have data returned ...
       response => {
         const html = response.data; // Get the HTML from the HTTP request
-        console.log(html);
+        //console.log(html);
         
         
         var m;
@@ -219,8 +154,8 @@ async function loadJSON(){
         do {
             m = sublink_regex.exec(html);
             if (m) {
-                console.log(m[0], m[1], m[2]);
-                prosJson.language.members.push({"name":m[2], "url": m[0]});
+                //console.log(m[0], m[1], m[2]);
+                prosJson.language.members.push({"name":m[3], "url": sublink_html + m[1]});
             }
         } while (m);
       });
@@ -229,6 +164,9 @@ async function loadJSON(){
 }
 
 //ParseJSON("Motor");
-loadJsonNew();
-console.log(json);
-console.log("Done");
+loadJsonNew().then(() => {
+  parseJSON("move_absolute");
+});
+
+
+
