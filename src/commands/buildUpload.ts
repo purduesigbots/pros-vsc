a13a956ad0193c79848521b25784c7239360a369
Andrew Lu <lu987@purdue.edu>
2024-03-28 17:57:09 -0400

    add errorCallback for when command fails

--- src/commands/buildUpload.ts
+++ src/commands/buildUpload.ts
@@ -3,19 +3,24 @@ import { window } from "vscode";
 import { upload } from "./upload";
 
 export const buildUpload = async () => {
+  var buildFailed = false;
   const buildCommandOptions: BaseCommandOptions = {
     command: "pros",
     args: ["make"],
     message: "Building Project",
     requiresProsProject: true,
     successMessage: "hidden",
+    errorCallback: () => {
+      buildFailed = true;
+    },
   };
   const buildCommand: BaseCommand = new BaseCommand(buildCommandOptions);
   try {
     await buildCommand.runCommand();
   } catch (err: any) {
     await window.showErrorMessage(err.message);
-    return;
   }
-  await upload();
+  if (!buildFailed) {
+    await upload();
+  }
 };
