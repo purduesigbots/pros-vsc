4cf8be2a510fee07df76acdd79a57dc50ee4f277
Jack Hughes <hughe307@purdue.edu>
2024-05-19 22:55:28 -0600

    run prettier on new file
    please pass ci check

--- src/commands/reset-conductor.ts
+++ src/commands/reset-conductor.ts
@@ -2,17 +2,20 @@ import * as vscode from "vscode";
 import { BaseCommand, BaseCommandOptions } from "./base-command";
 
 export const resetConductor = async () => {
-    const resetConductorCommandOptions: BaseCommandOptions = {
-        command: "pros",
-        args: ["c", "reset"], //--force not needed: Ben's base-commands class auto-handles warning + confirmation. 
-        message: "Resetting Conductor",
-        requiresProsProject: true,
-        successMessage: "Conductor Reset Successfully. Any templates and/or depots previously cached will need to be re-fetched.",
-    };
-    const resetConductorCommand: BaseCommand = new BaseCommand(resetConductorCommandOptions);
-    try {
-        await resetConductorCommand.runCommand();
-    } catch (err: any) {
-        await vscode.window.showErrorMessage(err.message);
-    }
+  const resetConductorCommandOptions: BaseCommandOptions = {
+    command: "pros",
+    args: ["c", "reset"], //--force not needed: Ben's base-commands class auto-handles warning + confirmation.
+    message: "Resetting Conductor",
+    requiresProsProject: true,
+    successMessage:
+      "Conductor Reset Successfully. Any templates and/or depots previously cached will need to be re-fetched.",
+  };
+  const resetConductorCommand: BaseCommand = new BaseCommand(
+    resetConductorCommandOptions
+  );
+  try {
+    await resetConductorCommand.runCommand();
+  } catch (err: any) {
+    await vscode.window.showErrorMessage(err.message);
+  }
 };
