f503cdd6df558aad861a730aaa725baa7d5403cf
meisZWFLZ <atcurtis08@gmail.com>
2024-10-07 00:27:50 -0700

    feat: :sparkles: support permanent user actions
    - make "Don't Show Again" work
    - add "Enable Autosave"
    - move permanent user actions into separate dialogue accessible with
      "More..." button

--- src/commands/build.ts
+++ src/commands/build.ts
@@ -4,11 +4,18 @@ import { output } from "../extension";
 
 /** Language IDs for C and C++ files */
 const C_CPP_LANG_IDS = ["cpp", "c"];
+/** Name of unsaved prompt setting  */
+const PROMPT_CONFIG_NAME = "Prompt On Unsaved Files";
 
 export const build = async () => {
-  // TODO: Share this functionality with the build and upload command
-  // TODO: Suggest autosave somehow.
+  // TODO: Share this functionality with the build and upload command to avoid code duplication
   // TODO: If autosave is enabled, attempt to save all unsaved files in order to prevent edge cases with vscode's autosave.
+  // TODO: Improve likelihood of user to find the "Enable Autosave" option
+
+  /** Whether we should we prompt the user when files are unsaved. */
+  const promptSetting = vscode.workspace
+    .getConfiguration("pros")
+    .get<boolean>(PROMPT_CONFIG_NAME);
 
   /** Unsaved document uris likely belonging to the user's program's source code. */
   const unsavedUris = vscode.workspace.textDocuments
@@ -21,7 +28,12 @@ export const build = async () => {
     .map((doc) => doc.uri);
 
   // If there are unsaved files, prompt the user to save them
-  if (unsavedUris.length > 0) {
+  if (unsavedUris.length > 0 && promptSetting) {
+    // TODO: Move this function outside of the parent function
+    function saveAll() {
+      return Promise.all(unsavedUris.map((uri) => vscode.workspace.save(uri)));
+    }
+
     const multipleUnsaved = unsavedUris.length > 1;
 
     // Change the message based on whether there are multiple unsaved files
@@ -34,35 +46,111 @@ export const build = async () => {
     }
 
     // TODO: Add a link to some documentation?
-    const message = `${problem} This may cause problems with building. [Learn more](https://pros.cs.purdue.edu/)`;
-
-    // Show the message and get the user's response
-    const response = await vscode.window.showErrorMessage(
-      message,
+    const message = `${problem} This may cause problems with building.`;
+    
+    // TODO: Move type nonsense outside of the parent function
+    // Define the possible user actions
+    const basicActions = [
       multipleUnsaved ? "Save All" : "Save",
       "Abort",
       "Ignore",
-      "Don't Ask Again"
-    );
+    ] as const;
+    const messageActions = [...basicActions, "More..."] as const;
+    const quickPickAction = [
+      ...basicActions,
+      "Enable Autosave",
+      "Don't Show Again",
+    ] as const;
 
-    // Handle user response
-    switch (response) {
-      case "Save":
-      case "Save All":
-        await Promise.all(unsavedUris.map((uri) => vscode.workspace.save(uri)));
-        break;
-      case "Abort":
-        return;
-      // User pressed the 'x' button on the message
-      case undefined:
-      case "Ignore":
-        break;
-      case "Don't Ask Again":
-        output.appendLine(
-          "User chose to ignore unsaved files warning for PROS build."
-        );
-        // TODO: add setting to disable this warning
-        break;
+    type MessageUserAction = (typeof messageActions)[number];
+    type QuickPickUserAction = (typeof quickPickAction)[number];
+    type UserAction = MessageUserAction | QuickPickUserAction;
+
+    const defaultAction: UserAction = "Ignore";
+
+    // Show the message and get the user's action
+    const action: MessageUserAction =
+      (await vscode.window.showErrorMessage(
+        message,
+        ...messageActions
+      )) /* If user presses x, default to Ignore */ ?? defaultAction;
+
+    // TODO: Move this function outside of the parent function
+    /**
+     * Perform the user action.
+     * @param action
+     * @returns Whether build process was aborted.
+     */
+    async function performUserAction(action: UserAction) {
+      switch (action) {
+        case "Save":
+        case "Save All":
+          await saveAll();
+          break;
+        case "Enable Autosave":
+          await vscode.commands.executeCommand(
+            "workbench.action.toggleAutoSave"
+          );
+          // If user enables autosave, we should also ensure to save all unsaved files
+          await saveAll();
+          break;
+        case "Abort":
+          return true;
+        case "Ignore":
+          break;
+        case "Don't Show Again":
+          vscode.workspace
+            .getConfiguration("pros")
+            .update(
+              PROMPT_CONFIG_NAME,
+              false,
+              vscode.ConfigurationTarget.Global
+            );
+          break;
+        case "More...":
+          const quickPickItems = [
+            {
+              label: "Save",
+              description:
+                "Save all unsaved files and continue with build process.",
+            },
+            {
+              label: "Abort",
+              description: "Abort build process.",
+            },
+            {
+              label: "Ignore",
+              description:
+                "Continue with build process without saving unsaved files.",
+            },
+            {
+              label: "Enable Autosave",
+              description:
+                "Continue with build process and enable vscode's autosave feature.",
+            },
+            {
+              label: "Don't Show Again",
+              description:
+                "Continue with build process and disable this warning in the future.",
+            },
+          ] satisfies Array<
+            { label: QuickPickUserAction } & vscode.QuickPickItem
+          >;
+          const newAction =
+            (
+              await vscode.window.showQuickPick(quickPickItems, {
+                ignoreFocusOut: true,
+              })
+            )?.label ?? defaultAction;
+          await performUserAction(newAction);
+          break;
+      }
+      return false;
+    }
+
+    const shouldAbort = await performUserAction(action);
+    if (shouldAbort) {
+      return;
     }
   }
   const buildCommandOptions: BaseCommandOptions = {
