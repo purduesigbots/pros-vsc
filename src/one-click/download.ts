e49616772b030e41bf88412d062282129cad3d39
Andrew Lu <lu987@purdue.edu>
2024-12-13 22:43:07 -0500

    :sparkles: Updating toolchain (#238)
    * get new toolchain installation working on linux
    
    * add progress bar, improve installation
    
    * switch to lzma-native
    
    * fix semver errors when cli/toolchain aren't installed
    
    * run prettier :)
    
    * remove hard-coded toolchain version

--- src/one-click/download.ts
+++ src/one-click/download.ts
@@ -2,8 +2,8 @@ import * as vscode from "vscode";
 import { window, ProgressLocation } from "vscode";
 var fetch = require("node-fetch");
 var admzip = require("adm-zip");
-var bunzip = require("seek-bzip");
 var tar = require("tar-fs");
+var lzma = require("lzma-native");
 import * as fs from "fs";
 import * as stream from "stream";
 import * as path from "path";
@@ -20,17 +20,18 @@ async function download(
 ) {
   // Check if file type is .tar.bz or .zip
 
-  const bz2 = downloadURL.includes(".bz2");
+  const xz = downloadURL.includes(".xz");
   await prosLogger.log("OneClick", `Downloading ${downloadURL}`);
   await prosLogger.log("OneClick", `Storage Path: ${storagePath}`);
 
   downloadName =
-    "Downloading: " + downloadName ??
-    (storagePath.includes("cli")
-      ? "PROS CLI"
-      : storagePath.includes("toolchain")
-      ? "PROS Toolchain"
-      : "VEX Vexcom");
+    "Downloading: " +
+    (downloadName ??
+      (storagePath.includes("cli")
+        ? "PROS CLI"
+        : storagePath.includes("toolchain")
+        ? "PROS Toolchain"
+        : "VEX Vexcom"));
 
   await window.withProgress(
     {
@@ -74,23 +75,24 @@ async function download(
       out.close();
     }
   );
-  return bz2;
+  return xz;
 }
 
 export async function extract(
   globalPath: string,
   storagePath: string,
-  bz2: boolean,
+  xzFile: boolean,
   extractName?: string
 ) {
   await prosLogger.log("OneClick", `Extracting ${storagePath}`);
   extractName =
-    "Installing: " + extractName ??
-    (storagePath.includes("cli")
-      ? "PROS CLI"
-      : storagePath.includes("toolchain")
-      ? "PROS Toolchain"
-      : "VEX Vexcom");
+    "Installing: " +
+    (extractName ??
+      (storagePath.includes("cli")
+        ? "PROS CLI"
+        : storagePath.includes("toolchain")
+        ? "PROS Toolchain"
+        : "VEX Vexcom"));
   await window.withProgress(
     {
       location: ProgressLocation.Notification,
@@ -107,66 +109,38 @@ export async function extract(
         extract.close();
       });
 
-      if (bz2) {
-        await prosLogger.log(
-          "OneClick",
-          `Reading compressed data from ${storagePath}`
-        );
-        // Read the contents of the bz2 file
-        var compressedData = await fs.promises.readFile(
-          path.join(globalPath, "download", storagePath)
-        );
-
-        // Decrypt the bz2 file contents.
-        let decompressedData;
-        await prosLogger.log("OneClick", `Decompressing ${storagePath}`);
-        try {
-          decompressedData = bunzip.decode(compressedData);
-        } catch (e: any) {
-          console.log(e);
-          await prosLogger.log(
-            "OneClick",
-            `Failed to decompress ${storagePath}`
-          );
-          vscode.window.showErrorMessage(
-            "An error occured while decoding the toolchain"
-          );
-        }
-
-        storagePath = storagePath.replace(".bz2", "");
-        await prosLogger.log(
-          "OneClick",
-          `Writing decompressed data to ${storagePath}`
-        );
-        await fs.promises.writeFile(
-          path.join(globalPath, "download", storagePath),
-          decompressedData
-        );
-        // Write contents of the decrypted bz2 into "sigbots.pros/download/filename.tar"
-
+      if (xzFile) {
         await new Promise(function (resolve, reject) {
           // Create our read stream
           prosLogger.log("OneClick", `Creating read stream for ${storagePath}`);
+          const stats = fs.statSync(
+            path.join(globalPath, "download", storagePath)
+          );
+          const totalSize = stats.size;
           read = fs.createReadStream(
             path.join(globalPath, "download", storagePath)
           );
+          var decompress = new lzma.createDecompressor();
+          decompress.on("data", (chunk: Buffer | string | any) => {
+            _progress.report({ increment: (chunk.length * 100) / totalSize });
+          });
           // Remove tar from the filename
-          storagePath = storagePath.replace(".tar", "");
+          storagePath = storagePath.replace(".tar.xz", "");
           // create our write stream
           prosLogger.log(
             "OneClick",
             `Extracting ${storagePath} to install folder`
           );
-          extract = tar.extract(path.join(globalPath, "install", storagePath));
+          extract = tar.extract(path.join(globalPath, "download", storagePath));
           // Pipe the read stream into the write stream
-          read.pipe(extract);
+          read.pipe(decompress).pipe(extract);
           // When the write stream ends, resolve the promise
           extract.on("finish", resolve);
           // If there's an error, reject the promise and clean up
           read.on("error", () => {
             prosLogger.log("OneClick", `Error occured for ${storagePath}`);
             fs.unlink(
-              path.join(globalPath, "install", storagePath),
+              path.join(globalPath, "download", storagePath),
               (_) => null
             );
             reject();
@@ -174,30 +148,15 @@ export async function extract(
         });
 
         const files = await fs.promises.readdir(
-          path.join(globalPath, "install")
+          path.join(globalPath, "download", storagePath)
         );
 
         for (const file of files) {
-          if (file.includes("toolchain")) {
-            await prosLogger.log("OneClick", `Finding toolchain files to move`);
-            const interfiles = await fs.promises.readdir(
-              path.join(globalPath, "install", file)
+          if (file.includes("arm-none-eabi")) {
+            await fs.promises.rename(
+              path.join(globalPath, "download", storagePath, file),
+              path.join(globalPath, "install", storagePath)
             );
-            for (const intfile of interfiles) {
-              if (intfile.includes("gcc-arm-none-eabi")) {
-                const toBringOut = await fs.promises.readdir(
-                  path.join(globalPath, "install", file, intfile)
-                );
-                for (const f of toBringOut) {
-                  await prosLogger.log("OneClick", `Moving ${f} to ${file}`);
-                  await fs.promises.rename(
-                    path.join(globalPath, "install", file, intfile, f),
-                    path.join(globalPath, "install", file, f)
-                  );
-                }
-              }
-            }
-            console.log(path.join(globalPath, "install", storagePath));
           }
         }
       } // if bz2
@@ -241,14 +200,14 @@ export async function downloadextract(
   name?: string
 ) {
   const globalPath = context.globalStorageUri.fsPath;
-  const bz2 = await download(
+  const xz = await download(
     globalPath,
     downloadURL,
     storagePath,
     name ?? undefined
   );
   console.log("download done");
-  await extract(globalPath, storagePath, bz2, name ?? undefined);
+  await extract(globalPath, storagePath, xz, name ?? undefined);
   console.log(`Finished Installing ${storagePath}`);
   window.showInformationMessage(`Finished Installing ${name ?? ""}`);
   return true;
