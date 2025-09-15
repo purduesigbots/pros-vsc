a91335702a49af0eef6aacb42a016d09d2500316
Kade-Zisko <blazeplays797@gmail.com>
2025-09-15 17:48:28 -0400

    Prompt only on PROs project and when autosave is off

--- src/extension.ts
+++ src/extension.ts
@@ -165,6 +165,43 @@ export async function activate(context: vscode.ExtensionContext) {
         terminal.sendText("pros build-compile-commands --no-analytics");
       });
       generateCCppFiles();
+      //Display pop-up to enable autosave
+      if (
+        vscode.workspace.getConfiguration("pros").get<boolean>("promptAutoSave") &&
+        vscode.workspace
+          .getConfiguration("files")
+          .get<string>("autoSave") === "off"
+      ) {
+        vscode.window
+          .showInformationMessage(
+            "Would you like to enable autosave for PROS projects?",
+            "This Project",
+            "All Projects",
+            "Not now",
+            "Never"
+          )
+          .then((selection) => {
+            if (selection === "This Project") {
+              vscode.workspace
+                .getConfiguration("files")
+                .update("autoSave", "afterDelay", false);
+              vscode.workspace
+                .getConfiguration("pros")
+                .update("promptAutoSave", false, false);
+            } else if (selection === "Never") {
+              vscode.workspace
+                .getConfiguration("pros")
+                .update("promptAutoSave", false, false);
+            } else if (selection === "All Projects") {
+              vscode.workspace
+                .getConfiguration("files")
+                .update("autoSave", "afterDelay", true);
+              vscode.workspace
+                .getConfiguration("pros")
+                .update("promptAutoSave", false, true);
+            }
+          });
+      }
     } else {
       chooseProject();
     }
@@ -183,40 +220,7 @@ export async function activate(context: vscode.ExtensionContext) {
     vscode.commands.executeCommand("pros.welcome");
   }
 
-  //Display pop-up to enable autosave
-  if (
-    vscode.workspace.getConfiguration("pros").get<boolean>("promptAutoSave")
-  ) {
-    vscode.window
-      .showInformationMessage(
-        "Would you like to enable autosave for PROS projects?",
-        "This Project",
-        "All Projects",
-        "Not now",
-        "Never"
-      )
-      .then((selection) => {
-        if (selection === "This Project") {
-          vscode.workspace
-            .getConfiguration("files")
-            .update("autoSave", "afterDelay", false);
-          vscode.workspace
-            .getConfiguration("pros")
-            .update("promptAutoSave", false, false);
-        } else if (selection === "Never") {
-          vscode.workspace
-            .getConfiguration("pros")
-            .update("promptAutoSave", false, false);
-        } else if (selection === "All Projects") {
-          vscode.workspace
-            .getConfiguration("files")
-            .update("autoSave", "afterDelay", true);
-          vscode.workspace
-            .getConfiguration("pros")
-            .update("promptAutoSave", false, true);
-        }
-      });
-  }
+
 
   // Set up all commands to run with the command blocker (see near the top of this file to understand what it does)
   // Commands with basic/default options:
